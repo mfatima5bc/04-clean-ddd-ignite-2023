98d3e022e31a8934f805d19e1b2373c6f7cfae19
Maria de Fátima <maria.cardoso@semantix.ai>
2023-04-22 00:07:28 -0300

    feat: :art: create getters and setters for all entities and configuration for vitest imports

--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -1,11 +1,15 @@
-import { Entity } from "../../core/entities/entity"
-import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { Entity } from "@/core/entities/entity"
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
 
 interface InstructorProps {
   name: string
 }
 
 export class Instructor extends Entity<InstructorProps> {
+  get name() {
+    return this.props.name
+  }
+  
   static create(
     props: InstructorProps, 
     id?: UniqueEntityID
