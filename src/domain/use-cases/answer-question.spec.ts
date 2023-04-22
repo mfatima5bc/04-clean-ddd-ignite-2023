935633e5425db63f4fa6e10fc6391238d0f2f91e
Maria de Fátima <maria.cardoso@semantix.ai>
2023-04-21 22:36:42 -0300

    feat: :sparkles: start building entities and a first use case for a forum.
    - add student entitie
    
    - add instructor entitie

---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,22 @@
+import { expect, test } from 'vitest'
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswerRepository } from '../repositories/answers-repository'
+import { Answer } from '../entities/answer'
+
+const fakeAnswersRepositorys: AnswerRepository = {
+  create: async (answer: Answer) => {
+    return;
+  }
+}
+
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepositorys)
+
+  const answer = await answerQuestion.execute({
+    questionId: '1',
+    instructorId: '1',
+    content: 'Nova resposta'
+  })
+
+  expect(answer.content).toEqual('Nova resposta')
+})
---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,26 @@
+import { Answer } from "../entities/answer"
+import { AnswerRepository } from "../repositories/answers-repository"
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string
+  questionId: string
+  content: string
+}
+
+export class AnswerQuestionUseCase {
+  constructor(
+    private answersRepository: AnswerRepository,
+  ) {}
+
+  async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+    const answer = new Answer({
+      content, 
+      authorId: instructorId, 
+      questionId
+    })
+
+    await this.answersRepository.create(answer)
+
+    return answer
+  }
+}
