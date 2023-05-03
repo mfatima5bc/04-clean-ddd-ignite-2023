ff58c8afc68955d7d4e2d2127a359f0ce55b883a
Maria de Fátima <maria.cardoso@semantix.ai>
2023-05-03 14:53:53 -0300

    fix: ortograph errors

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -2,14 +2,14 @@ import { AnswerQuestionUseCase } from './answer-question'
 import { AnswerRepository } from '../repositories/answers-repository'
 import { Answer } from '../entities/answer'
 
-const fakeAnswersRepositorys: AnswerRepository = {
+const fakeAnswersRepositories: AnswerRepository = {
   create: async (answer: Answer) => {
     return;
   }
 }
 
 test('create an answer', async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepositorys)
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepositories)
 
   const answer = await answerQuestion.execute({
     questionId: '1',
