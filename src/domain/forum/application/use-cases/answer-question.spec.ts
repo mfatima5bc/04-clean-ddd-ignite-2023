1bed1a208a0bf2ad75da38b15cf103abfd928d25
Maria de Fátima <maria.defatimabarbosacardoso@gmail.com>
2023-05-04 23:42:47 -0300

    style: refactor structure

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,6 +1,6 @@
 import { AnswerQuestionUseCase } from './answer-question'
 import { AnswerRepository } from '../repositories/answers-repository'
-import { Answer } from '../entities/answer'
+import { Answer } from '../../enterprise/entities/answer'
 
 const fakeAnswersRepositories: AnswerRepository = {
   create: async (answer: Answer) => {},
