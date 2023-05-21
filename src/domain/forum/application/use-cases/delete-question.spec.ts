418cb223a67bd2f6819c67d060827e35284e6ad8
Maria de Fátima <maria.defatimabarbosacardoso@gmail.com>
2023-05-21 11:52:37 -0300

    feat: :sparkles: add delete question
    the user should be able to delete your won question

---
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -0,0 +1,50 @@
+import { InMemoryQuestionRepository } from 'test/in-memory-question-repository'
+import { DeleteQuestionUseCase } from './delete-question'
+import { makeQuestion } from 'test/factories/make-question'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let inMemoryQuestionRepository: InMemoryQuestionRepository
+let sut: DeleteQuestionUseCase
+
+describe('Delete Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionRepository = new InMemoryQuestionRepository()
+    sut = new DeleteQuestionUseCase(inMemoryQuestionRepository)
+  })
+
+  it('should be able to delete a question', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('question-1'),
+    )
+
+    await inMemoryQuestionRepository.create(newQuestion)
+
+    await sut.execute({
+      authorId: 'author-1',
+      questionId: 'question-1',
+    })
+
+    expect(inMemoryQuestionRepository.items).toHaveLength(0)
+  })
+
+  it('should not be able to delete a question from another user', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('question-1'),
+    )
+
+    await inMemoryQuestionRepository.create(newQuestion)
+
+    expect(async () => {
+      return await sut.execute({
+        authorId: 'author-2',
+        questionId: 'question-1',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
--- test/in-memory-question-repository.ts
+++ test/in-memory-question-repository.ts
@@ -4,6 +4,19 @@ import { Question } from '@/domain/forum/enterprise/entities/question'
 export class InMemoryQuestionRepository implements QuestionRepository {
   public items: Question[] = []
 
+  async findById(id: string): Promise<Question | null> {
+    const question = this.items.find((item) => item.id.toString() === id)
+
+    if (!question) return null
+
+    return question
+  }
+
+  async delete(question: Question): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+    this.items.splice(itemIndex, 1)
+  }
+
   async findBySlug(slug: string): Promise<Question | null> {
     const question = this.items.find((item) => item.slug.value === slug)
 
