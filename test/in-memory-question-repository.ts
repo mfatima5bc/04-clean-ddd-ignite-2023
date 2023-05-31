aecd5abd9d8033f84f317c71cff6bbb6f90763d3
Maria de Fátima <maria.cardoso@semantix.ai>
2023-05-30 23:07:14 -0300

    refactor: :bug: use sort into a new array instead to change the original array

--- test/in-memory-question-repository.ts
+++ test/in-memory-question-repository.ts
@@ -6,7 +6,7 @@ export class InMemoryQuestionRepository implements QuestionRepository {
   public items: Question[] = []
 
   async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
-    const questions = this.items
+    const questions = [...this.items]
       .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
       .slice((page - 1) * 20, page * 20)
     return questions
